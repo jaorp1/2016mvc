9ca1de416dd2b4d87c59cbcdeb1e326a79ecdaec
HB6018\user <jaorp1@gmail.com>
2016-11-17 15:07:05 +0900

    스프링 따라하기 기본세팅 ^_^

---
+++ ex02/src/com/mvc2/core/DispatcherServlet.java
@@ -0,0 +1,25 @@
+package com.mvc2.core;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		dodispatcher(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		dodispatcher(request,response);
+	}
+	protected void dodispatcher(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/page/index.jsp").forward(request, response);
+	}
+
+}
